3b9999f1a5699a561565fca1bb9ffbea61df0b2b
kiet <buikiet16801@gmail.com>
2022-08-12 16:39:13 -0700

    skill

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,4 @@
+import images from "../constains/images";
 export const projects = [
     {
         id:1,
@@ -37,4 +38,60 @@ export const projects = [
     },
 ];
 
-export default {projects}
+export const skills = [
+    {
+        id:1,
+        image:images.html,
+        name:'Html 5'
+    },
+    {
+        id:2,
+        image:images.css,
+        name:'Css 3'
+    },
+    {
+        id:3,
+        image:images.js,
+        name:'Javascript'
+    },
+    {
+        id:4,
+        image:images.bootstrap,
+        name:'Bootstrap'
+    },
+    {
+        id:5,
+        image:images.sass,
+        name:'Sass'
+    },
+    {
+        id:6,
+        image:images.tailwind,
+        name:'Tailwind Css'
+    },
+    {
+        id:7,
+        image:images.jquery,
+        name:'Jquery'
+    },
+    {
+        id:8,
+        image:images.typescript,
+        name:'Typescript'
+    },
+    {
+        id:9,
+        image:images.reactjs,
+        name:'React Js'
+    },
+    {
+        id:10,
+        image:images.reactnative,
+        name:'React Native'
+    },
+]
+
+export default {
+    projects,
+    skills
+}
